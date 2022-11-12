acd8e967fc801b8b728f9920a67f865e9932e2f8
dicosmode <ezequiel.dicosmo@gmail.com>
2022-11-11 23:30:24 -0300

    delete comments

--- src/main/java/com/capitole/capitolTest/core/entities/Price.java
+++ src/main/java/com/capitole/capitolTest/core/entities/Price.java
@@ -24,17 +24,3 @@ public class Price {
     private float price;
     private String curr;
 }
-
-
-/*
-*                                       price_id INT NOT NULL AUTO_INCREMENT,
-                                      brand_id INT NOT NULL,
-                                      start_date DATETIME NOT NULL,
-                                      end_date DATETIME NOT NULL,
-                                      price_list INT NOT NULL,
-                                      product_id INT NOT NULL,
-                                      priority INT NOT NULL,
-                                      price DECIMAL(20,2) NOT NULL,
-                                      curr VARCHAR(4) NOT NULL,
-*
-* */
